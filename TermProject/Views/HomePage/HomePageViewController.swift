301266a22dd6845b6d0fe5d4fb98690e15f9c6d4
Sai Aik Sei Mouk <saiseimouk.mdy@gmail.com>
2024-09-05 19:13:22 +0700

    Add Home page scaffold

---
+++ TermProject/Views/HomePage/HomePageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageViewController.swift
+//  TermProject
+//
+//  Created by Sei Mouk on 5/9/24.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "App name"
+        
+        
+    }
+}
