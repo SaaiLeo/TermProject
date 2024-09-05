2258fdf3c905e12fb121eadfa49e1c504fad4ae6
Sai Aik Sei Mouk <saiseimouk.mdy@gmail.com>
2024-09-05 22:28:01 +0700

    Add BestSeller Feature to Home Page

---
+++ TermProject/Models/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  TermProject
+//
+//  Created by Sei Mouk on 5/9/24.
+//
+
+import Foundation
+
+struct Menu {
+    let name: String
+    let price: String
+    let image: String
+    let category: String
+    let popularity: String
+}
