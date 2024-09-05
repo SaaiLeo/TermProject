90b93c023877310fc620167c51dfeeafae4c68d5
saithaw23 <saithaw619@gmail.com>
2024-09-06 00:13:08 +0700

    added Login Page

--- TermProject/Views/LoginPage/LoginPageViewController.swift
+++ TermProject/Views/LoginPage/LoginPageViewController.swift
@@ -6,11 +6,52 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginPageViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationController?.navigationBar.tintColor = UIColor.white
+        
+//        if FirebaseAuth.Auth.auth().currentUser != nil {
+//            navigateToHomeScreen()
+//        }       uncommand this after implementing homescreen
     }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func loginBtnClicked(_ sender: UIButton) {
+        guard let email = emailTF.text else {return}
+        guard let password = passwordTF.text else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { firebaseResult, error in
+            if let e = error {
+                self.showAlert(message: e.localizedDescription)
+            } else {
+//                self.navigateToHomeScreen()
+                print("signIn complete")
+            }
+        }
+    }
+    
+    @IBAction func signupBtnClicked(_ sender: UIButton) {
+        let signupPage = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "signuppage") as! SignUpViewController
+        navigationController?.pushViewController(signupPage, animated: true)
+    }
+    
+//    func navigateToHomeScreen(){
+//        .........
+//    }
+    
+    
 }
--- TermProject/Views/LoginPage/SignUpViewController.swift
+++ TermProject/Views/LoginPage/SignUpViewController.swift
@@ -6,9 +6,16 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class SignUpViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +23,54 @@ class SignUpViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func createBtnClicked(_ sender: UIButton) {
+        guard let name = usernameTF.text, !name.isEmpty else {
+            showAlert(message: "Please enter your name.")
+            return
+        }
+        guard let email = emailTF.text, !email.isEmpty else {
+            showAlert(message: "Please enter your email.")
+            return
+        }
+        guard let password = passwordTF.text, !password.isEmpty else {
+            showAlert(message: "Please enter your password.")
+            return
+        }
+        
+        let alert = UIAlertController(title: "Create Account", message: "Would you like to create an account?", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: { _ in
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    self.showAlert(message: e.localizedDescription)
+                } else if let authResult = authResult {
+                    let db = Firestore.firestore()
+                    db.collection("users").document(authResult.user.uid).setData([
+                        "name": name,
+                        "email": email
+                    ]) { err in
+                        if let err = err {
+                            print("Error writing document: \(err)")
+                        } else {
+                            print("account successfully created")
+                            // go to homescreen
+                        }
+                    }
+                }
+            }
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+        
     }
-    */
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 
 }
