25759488966e0fafd097d83af2e9cc016449b336
saithaw23 <saithaw619@gmail.com>
2024-09-06 00:09:09 +0700

    added loginPages

---
+++ TermProject/Views/LoginPage/SignUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpViewController.swift
+//  TermProject
+//
+//  Created by SaiThaw- on 5/9/2567 BE.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
