fdcfe51e9d038767b60b0137d0fc138c99259827
saithaw23 <saithaw619@gmail.com>
2024-09-06 19:32:15 +0700

    Add History Page scaffold

---
+++ TermProject/HistoryPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HistoryPageViewController.swift
+//  TermProject
+//
+//  Created by SaiThaw- on 6/9/2567 BE.
+//
+
+import UIKit
+
+class HistoryPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
