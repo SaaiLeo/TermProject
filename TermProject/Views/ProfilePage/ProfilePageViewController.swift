cb4e1174741c76fcd22d10534ac9625b1b1e9e93
saithaw23 <saithaw619@gmail.com>
2024-09-14 20:36:33 +0700

    initialize profile page

--- TermProject/Views/ProfilePage/ProfilePageViewController.swift
+++ TermProject/Views/ProfilePage/ProfilePageViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class ProfilePageViewController: UIViewController {
 
@@ -13,16 +15,35 @@ class ProfilePageViewController: UIViewController {
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var profileUsernameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if let user = Auth.auth().currentUser {
+            let db = Firestore.firestore()
+            let userId = user.uid
+            
+            db.collection("users").document(userId).getDocument { (document, error) in
+                if let document = document, document.exists {
+                    let data = document.data()
+                    let name = data?["name"] as? String
+                    let email = data?["email"] as? String
+                    self.usernameLabel.text = name
+                    self.profileUsernameLabel.text = name
+                    self.emailLabel.text = email
+                } else {
+                    print("Document does not exist")
+                }
+            }
+        }
     }
     
-    private func setup(_ user: User){
-        userImageView.image = UIImage(named: user.image)
-        usernameLabel.text = user.name
-        emailLabel.text = user.email
-        phoneLabel.text = user.phone
+    @IBAction func logoutBtnClicked(_ sender: UIButton) {
     }
+    
+    
+    
+    
+    
 }
